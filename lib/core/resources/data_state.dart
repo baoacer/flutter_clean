c15ec4e5a10d141c2f91bffacd8dfe323985e6b7
baoacer <nguyenquocbaotu@gmail.com>
2025-01-02 16:36:33 +0700

    generate project

---
+++ lib/core/resources/data_state.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: unused_import
+import 'package:dio/dio.dart';
+
+abstract class DataState<T> {
+  final T? data;
+  final DioException? error;
+
+  const DataState({this.data, this.error});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioException error) : super(error: error);
+}
